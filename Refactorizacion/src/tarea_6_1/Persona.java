dde11ee441ce840f2300355a599171cc7a8869f8
Christian <christianrmch@outlook.es>
2023-05-11 19:19:45 +0200

    Ejericio 6.1 sin refactorizar, la refactorizacion estará en otra rama llamada primerEjercicio

---
+++ Refactorizacion/src/tarea_6_1/Persona.java
@@ -0,0 +1,17 @@
+package tarea_6_1;
+
+public class Persona {
+    String numeroDeTelefono;
+    public Persona(String numeroDeTelefono){
+        super();
+        this.numeroDeTelefono = numeroDeTelefono;
+    }
+
+    public String getNumeroDeTelefono(){
+        return this.numeroDeTelefono;
+    }
+
+    public void setNumeroDeTelefono(String numeroDeTelefono){
+        this.numeroDeTelefono = numeroDeTelefono;
+    }
+}
---
+++ Refactorizacion/src/tarea_6_1/Prestamo.java
@@ -0,0 +1,4 @@
+package tarea_6_1;
+
+public class Prestamo {
+}
