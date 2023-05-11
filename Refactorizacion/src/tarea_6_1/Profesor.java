c0defb768874ebfb7fb6525744f51125fa82caaf
Christian <christianrmch@outlook.es>
2023-05-11 20:48:04 +0200

    primer ejercicio finalizado

--- Refactorizacion/src/tarea_6_1/Profesor.java
+++ Refactorizacion/src/tarea_6_1/Profesor.java
@@ -1,14 +1,18 @@
 package tarea_6_1;
+import java.util.ArrayList;
+
 public class Profesor extends Persona{
     String nombre;
     int edad;
     String numeroDeTelefono;
-    List<Prestamo> prestamos;
+
+    //Cambio List por un ArrayList para no tener que implementar todos los métodos de la interfaz List en la clase.
+    ArrayList<Prestamo> prestamos;
     public Profesor(String numeroDeTelefono){
         super(numeroDeTelefono);
     }
 
-    //usamos this. para acceder a los atributos de la propia clase.
+    //Uso this. para acceder a los atributos de la propia clase.
     public void printInformacionPersonal(){
         System.out.println("Nombre: " + this.nombre);
         System.out.println("Edad: " + this.edad);
@@ -18,6 +22,7 @@ public class Profesor extends Persona{
         System.out.println("Nombre: " + this.nombre);
         System.out.println("Edad: " + this.edad);
         System.out.println("Numero de teléfono: " + this.numeroDeTelefono);
+        //Dejo el foreach en una sola línea
         for (Prestamo p : prestamos) System.out.println(p);
     }
 }
